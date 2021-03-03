346299dd1f60db77e1c6ce8ff4a992cdbd70b00c
akhilomar <akhilomar555@gmail.com>
2021-03-03 14:05:22 +0530

    12

---
+++ HashMap/12distinct_in_every_ksizewin.cpp
@@ -0,0 +1,35 @@
+#include<bits/stdc++.h>
+using namespace std;
+vector<int> processor(vector<int> a, int k){
+    unordered_map<int, int> m;
+    int counter = 0;
+    vector<int> ans;
+    for(int i = 0; i < k; i++){
+        if(m[a[i]] == 0){
+            counter++;
+        }
+            m[a[i]]+=1;
+        
+    }
+    ans.push_back(counter);
+    for(int i = k; i < a.size(); i++){
+        if(m[a[i - k]] == 1)
+            counter--;
+        m[a[i-k]]-=1;
+        if(m[a[i]] == 0){
+            counter++;
+            
+        }
+        m[a[i]]++;
+        ans.push_back(counter);
+    }
+    return ans;
+}
+int main(void){
+    vector<int> a = {2,3,2,4,5,5,6,7,8,5,6};
+    int k = 3;
+    vector<int> ans = processor(a, k);
+    for(auto i : ans){
+        cout<<i<<endl;
+    }
+}
