5fa6d67be91a2bbc5ff8c0f87cea9b3b73b69c74
akhilomar <akhilomar555@gmail.com>
2021-04-01 23:22:18 +0530

    bubble sort

---
+++ Sorting/Bubblesort.cpp
@@ -0,0 +1,12 @@
+Q: Bubble Sort
+
+void BubbleSort(vector<int>& a){
+    for(int i = 0; i < a.size() - 1; i++){
+        for(int j = 0; j < a.size() - i - 1; j++){
+            if(a[j] > a[j + 1])
+                swap(a[j], a[j+1])
+        }
+    }
+}
+
+It is Sorting from end
